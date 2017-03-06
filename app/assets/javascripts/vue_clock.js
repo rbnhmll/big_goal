94414abea0f9385aa1502224de03e7bad82b32b5
Robin Hamill <hamillrobin@gmail.com>
2017-03-06 15:02:30 -0500

    Move Vue into JS file

---
+++ app/assets/javascripts/vue_clock.js
@@ -0,0 +1,48 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    secondsInDay: 86400,
+    secondsInHour: 3600,
+    url: window.location.href + ".json",
+    deadlineSecs: 0,
+    nowSeconds: Date.now()
+  },
+  methods: {
+    getNow: function() {
+      setInterval(function(){
+        app.nowSeconds = Date.now();
+        if (this.secondsLeft <= 0) {
+          clearInterval();
+        }
+      }, 1000);
+    }
+  },
+  computed: {
+    secondsLeft: function() {
+      return Math.round( (this.deadlineSecs - this.nowSeconds) / 1000 );
+    },
+    days: function() {
+      return Math.floor(this.secondsLeft / this.secondsInDay)
+    },
+    hours: function() {
+      return Math.floor( (this.secondsLeft % this.secondsInDay) / 60 / 60 );
+    },
+    minutes: function() {
+      return Math.floor( (this.secondsLeft % this.secondsInHour ) / 60);
+    },
+    seconds: function() {
+      return Math.floor(this.secondsLeft % 60);
+    }
+  },
+  mounted: function() {
+    this.getNow();
+    axios.get(this.url)
+    .then(function (response) {
+      var data = response.data.deadline;
+      app.deadlineSecs = new Date(data).getTime();
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  }
+});
