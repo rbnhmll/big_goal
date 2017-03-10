90e693a11619cde1919f8107fd98ec75569b3cdf
Robin Hamill <hi@robinhamill.ca>
2017-03-10 14:40:38 -0500

    Fix new profile error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def authenticate_has_profile
 		if !current_user.profile.present?
 			@profile = Profile.new(user: current_user)
-			if @profile.save
+			if @profile.save!
 				redirect_to edit_profile_path(@profile),
 				notice: 'Please fill out a profile before continuing'   
 			end
