400bcd33152d19dadf0a82b66e57a74cd75b9ed7
Robin Hamill <hi@robinhamill.ca>
2017-03-10 13:23:08 -0500

    More Style fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,15 +2,13 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
 
-
-  # GET /profiles/1
-  # GET /profiles/1.json
-  def show
-  end
-
   # GET /profiles/1/edit
   def edit
     set_profile
+  end
+
+  def show
+      # Show is still being used for an API call to pull in Profile name
   end
 
   # POST /profiles
@@ -21,7 +19,7 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Profile was successfully created.' }
         format.json { render :show, status: :created, location: @profile }
       else
         format.html { render :edit }
@@ -35,7 +33,7 @@ class ProfilesController < ApplicationController
   def update
     respond_to do |format|
       if @profile.update(profile_params)
-        format.html { redirect_to @profile, notice: 'Profile was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @profile }
       else
         format.html { render :edit }
@@ -49,7 +47,7 @@ class ProfilesController < ApplicationController
   def destroy
     @profile.destroy
     respond_to do |format|
-      format.html { redirect_to profiles_url, notice: 'Profile was successfully destroyed.' }
+      format.html { redirect_to root_path, notice: 'Profile was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
