b4dbc1718078e610257fd51a06898d9320e20b39
Robin Hamill <hamillrobin@gmail.com>
2017-03-11 13:16:02 -0500

    Move authenticate_is_admin! into helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def authenticate_is_admin!
-  	if current_user.admin?
-		  return true
-		else
-			redirect_to custom_messages_path
-		end
-  end
+  
 end
--- app/helpers/default_messages_helper.rb
+++ app/helpers/default_messages_helper.rb
@@ -1,2 +1,11 @@
 module DefaultMessagesHelper
+
+	def authenticate_is_admin!
+  	if current_user.admin?
+		  return true
+		else
+			redirect_to custom_messages_path
+		end
+  end
+	
 end
