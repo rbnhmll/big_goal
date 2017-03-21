dac54e9ef6570a082b9ed836137c793d5bfcf417
Robin Hamill <hi@robinhamill.ca>
2017-03-21 19:17:58 -0400

    Add mail host URL Still need to set up Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
 
-  # config.action_mailer.default_url_options = { host: 'http://www.biggoal.com' }
+  config.action_mailer.default_url_options = { host: 'https://biggoal.herokuapp.com' }
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
